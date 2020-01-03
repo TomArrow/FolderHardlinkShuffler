100d734921b3527a11f328caf01fe399dfa7a33f
Hans Doof <hans@doof.de>
2020-01-03 16:23:54 +0100

    Works, what more can I say

---
+++ FolderHardlinkShuffler/Helpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderHardlinkShuffler
+{
+    static class Helpers
+    {
+
+        // Fisher-Yates algorithm AKA the Knuth Shuffle
+        // Found here: https://stackoverflow.com/questions/108819/best-way-to-randomize-an-array-with-net
+        public static void Shuffle<T>(this Random rng, T[] array)
+        {
+            int n = array.Length;
+            while (n > 1)
+            {
+                int k = rng.Next(n--);
+                T temp = array[n];
+                array[n] = array[k];
+                array[k] = temp;
+            }
+        }
+    }
+}
--- FolderHardlinkShuffler/MainWindow.xaml.cs
+++ FolderHardlinkShuffler/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Ookii.Dialogs;
+using Ookii.Dialogs.Wpf;
+using System.IO;
+using Microsoft.Win32;
 
 namespace FolderHardlinkShuffler
 {
@@ -23,6 +27,91 @@ namespace FolderHardlinkShuffler
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        string srcFolder = "";
+        string trgtFolder = "";
+
+        private void BtnSourceFolder_Click(object sender, RoutedEventArgs e)
+        {
+            VistaFolderBrowserDialog fbd = new VistaFolderBrowserDialog();
+            if(fbd.ShowDialog() == true)
+            {
+                srcFolder = fbd.SelectedPath;
+                sourceFolder.Text = srcFolder;
+                enableButton();
+            }
+        }
+
+        private void BtnTargetFolder_Click(object sender, RoutedEventArgs e)
+        {
+            VistaFolderBrowserDialog fbd = new VistaFolderBrowserDialog();
+            if (fbd.ShowDialog() == true)
+            {
+                trgtFolder = fbd.SelectedPath;
+                targetFolder.Text = trgtFolder;
+                enableButton();
+            }
+        }
+
+        private void enableButton()
+        {
+
+            if (trgtFolder == "" || srcFolder == "")
+            {
+                // Nope.
+                return;
+            } else
+            {
+                btnDoit.IsEnabled = true;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(trgtFolder == "" || srcFolder == "")
+            {
+                // Nope.
+                return;
+            }
+
+        }
+
+        private void BtnDoit_Click(object sender, RoutedEventArgs e)
+        {
+            string[] files = Directory.GetFiles(srcFolder);
+            var rng = new Random();
+            rng.Shuffle(files);
+
+            int zeroFill = int.Parse(txtFillZeros.Text);
+
+            string prefix = txtPrefix.Text;
+
+            string batchfile = "";
+
+            int index = 0;
+
+            batchfile += "SET srcFolder="+srcFolder+"\n";
+            batchfile += "SET dstFolder="+trgtFolder+"\n";
+
+            string rawFileName;
+
+            foreach(string filename in files)
+            {
+                rawFileName = System.IO.Path.GetFileName(filename);
+                batchfile += "mklink /h \"%dstFolder%" + System.IO.Path.DirectorySeparatorChar + prefix  + (index++).ToString("D"+zeroFill) + System.IO.Path.GetExtension(filename)+"\" \"%srcFolder%" + System.IO.Path.DirectorySeparatorChar + rawFileName + "\" " +"\n";
+            }
+
+            batchfile += "pause\n";
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Batch File | *.bat";
+            sfd.DefaultExt = "bat";
+            sfd.FileName = "shuffledHardlinksCreate.bat";
+            if(sfd.ShowDialog() == true)
+            {
+                File.WriteAllText(sfd.FileName, batchfile);
+            }
         }
     }
 }
